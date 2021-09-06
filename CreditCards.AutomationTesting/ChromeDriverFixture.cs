dffa2917b3c9395df68e085aead271d2b30b03fe
nicoscandolo <nico.scandolo@live.com>
2021-09-06 20:21:47 -0300

    Create Automation Project withing the solution /n Add Selenium and XUnit Packages

---
+++ CreditCards.AutomationTesting/ChromeDriverFixture.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCards.AutomationTesting
+{
+    public class ChromeDriverFixture
+    {
+        public IWebDriver Driver { get; private set; }
+
+        public ChromeDriverFixture()
+        {
+            Driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            Driver.Dispose();
+        }
+
+    }
+}
